3b1dd5b02602298bfb4e299b29eec5978ffade05
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-27 03:42:16 +0200

    :sparkles: Created test controller :sparkles: Implemented ProtectedRoute to force auth

---
+++ src/Controller/Api/ClientController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/client')]
+class ClientController extends AbstractController implements ProtectedRoute
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ClientController.php',
+        ]);
+    }
+}
