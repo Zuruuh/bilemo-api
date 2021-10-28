0745c626b4234183d71d2819e8d6d96472941035
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-28 03:31:01 +0200

    :sparkles: Generated fake products using faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,8 +3,10 @@
 namespace App\DataFixtures;
 
 use App\Entity\Client;
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -16,8 +18,10 @@ class AppFixtures extends Fixture
         $this->hasher = $hasher;
     }
 
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $em)
     {
+        $faker = Factory::create('fr_FR');
+
         $client = new Client();
         $password = $this->hasher->hashPassword($client, 'admin');
 
@@ -26,7 +30,21 @@ class AppFixtures extends Fixture
             ->setEmail('younesziadi@outlook.fr')
             ->setPassword($password);
 
-        $manager->persist($client);
-        $manager->flush();
+        for ($i = 0; $i < 110; ++$i) {
+            $product = (new Product())
+                ->setName($faker->word())
+                ->setOs($faker->word())
+                ->setManufacturer($faker->word() . " " . $faker->word())
+                ->setStorage($faker->numberBetween(2, 256))
+                ->setPrice($faker->numberBetween(8000, 200000))
+                ->setStock($faker->numberBetween(0, 32));
+
+            $em->persist($product);
+        }
+
+        $em->flush();
+
+        $em->persist($client);
+        $em->flush();
     }
 }
