f2dd2031d0984f621c7d9bcbc8ec3aee13ae086d
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-28 03:48:53 +0200

    :sparkles: Created findOneByWithArray & findByCursor methods

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -14,9 +14,49 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class ProductRepository extends ServiceEntityRepository
 {
+    const LIMIT = 10;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    /**
+     * Returns a set amount of products based on a set cursor
+     * 
+     * @param int $cursor The current cursor
+     * 
+     * @return Product[]|null
+     */
+    public function findByCursor(int $cursor): array
+    {
+        return $this->createQueryBuilder('p')
+            ->orderBy('p.id', 'ASC')
+            ->setMaxResults(self::LIMIT)
+            ->setFirstResult($cursor)
+            ->getQuery()
+            ->getArrayResult();
+    }
+
+    /**
+     * Returns a product as an associative array based on custom params
+     * 
+     * @param array $params The parameters to pass to the query
+     * 
+     * @return Product|null
+     */
+    public function findOneByWithArray(array $params): array
+    {
+        $qb = $this->createQueryBuilder('p');
+
+        $i = 0;
+        foreach ($params as $key => $param) {
+            $qb
+                ->andWhere("p.$key = :param$i")
+                ->setParameter(":param$i", $param);
+            ++$i;
+        }
+        return $qb->getQuery()->getArrayResult();
     }
 
     // /**
