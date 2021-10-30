219f2d444c492be1bfadab91bebed82587a5c6c5
Younès Ziadi <ziadi.mail.pro@gmail.com>
2021-10-30 06:53:35 +0200

    ✨ Created applyQueryBuilderParams method to dynamically add "where" clauses to doctrine query builder

--- src/Repository/FullEntityRepository.php
+++ src/Repository/FullEntityRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Product;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Exception;
 
 /**
@@ -13,36 +14,39 @@ use Exception;
  */
 abstract class FullEntityRepository extends ServiceEntityRepository
 {
-    const LIMIT = null;
+    const CURSOR_PAGINATION_LIMIT = null;
+    const ENTITY = 'entity';
 
-    const INVALID_LIMIT = "Entity pagination limit is set to null. Did you create a 'LIMIT' constant in your %s class ?";
-
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Product::class);
-    }
+    const INVALID_LIMIT = "Entity pagination limit is set to null. Did you create a 'CURSOR_PAGINATION_LIMIT' constant in your %s class ?";
 
     /**
      * Returns a set amount of the set entity based on passed cursor
      * 
-     * @param int $cursor The current cursor
+     * @param int   $cursor The current cursor
+     * @param array $params The query parameters
      * 
      * @return Entity[]|null
      */
-    public function findByCursor(int $cursor): array|null
+    public function findByCursor(int $cursor, array $params = []): array|null
     {
-        $limit = static::LIMIT;
+        $limit = static::CURSOR_PAGINATION_LIMIT;
         if (!$limit) {
             throw new Exception(
                 sprintf(self::INVALID_LIMIT, static::class)
             );
         }
-        return $this->createQueryBuilder('e')
-            ->orderBy('e.id', 'ASC')
+        $qb = $this->applyQueryBuilderParams(
+            $this->createQueryBuilder(static::ENTITY),
+            $params
+        );
+
+        $query = $qb->orderBy(static::ENTITY . '.id', 'ASC')
             ->setMaxResults($limit)
             ->setFirstResult($cursor)
-            ->getQuery()
-            ->getArrayResult();
+            ->getQuery();
+
+        return $query->getArrayResult();
+        //->getArrayResult();
     }
 
     /**
@@ -50,19 +54,35 @@ abstract class FullEntityRepository extends ServiceEntityRepository
      * 
      * @param array $params The parameters to pass to the query
      * 
-     * @return [Entity]|null
+     * @return array<Entity>|null
      */
-    public function findOneByWithArray(array $params): array|null
+    public function findOneByWithArray(array $params)
     {
-        $qb = $this->createQueryBuilder('e');
+        $qb = $this->applyQueryBuilderParams(
+            $this->createQueryBuilder(static::ENTITY),
+            $params
+        );
 
+        return $qb->getQuery()->getArrayResult();
+    }
+
+    /**
+     * Applies an array of params to a query builder
+     * 
+     * @param QueryBuilder $qb     The query builder to apply the params to
+     * @param array        $params The params to apply to the query builder
+     * 
+     * @return QueryBuilder
+     */
+    private function applyQueryBuilderParams(QueryBuilder $qb, array $params): QueryBuilder
+    {
         $i = 0;
         foreach ($params as $key => $param) {
             $qb
-                ->andWhere("e.$key = :param$i")
+                ->andWhere(static::ENTITY . ".$key = :param$i")
                 ->setParameter(":param$i", $param);
             ++$i;
         }
-        return $qb->getQuery()->getArrayResult();
+        return $qb;
     }
 }
